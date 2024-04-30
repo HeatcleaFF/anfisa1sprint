f2cb8d5d2df54bfc00e01ee917444c94abc9ba2c
Anton Kovalenko <kovalenko.a.a02@gmail.com>
2024-04-30 22:41:11 +0500

    Django 1 sprint.

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -6,6 +6,10 @@ SECRET_KEY = 'django-insecure-oy%f52n92s=%3&b3dh&h)f(b4ag^_z*&3w+ooo!sy-g=449bwn
 
 DEBUG = True
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+]
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
